6962ed99466f88d587ac5667e63010d9d0670065
Selvin Mayes <selvinmayes@live.com>
2024-02-02 22:50:16 -0500

    fixed width in nav links

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -44,48 +44,57 @@ const NavLinks = (props: Props) => {
 
   return (
     <nav className="space-y-8 font-bold text-indigo-600 hidden lg:block scroll-smooth">
-      <Link href="#about-me" className="flex items-center group space-x-4">
-        <span
-          className={`group-hover:w-20 group-hover:h-1 transition-all bg-indigo-600 group-hover:opacity-100 ${
-            active === 1 ? "w-20 h-1 opacity-100" : "w-8 h-0.5 opacity-70"
-          }`}
-        />
-        <span
-          className={`transition-transform group-hover:opacity-100 ${
-            active === 1 ? "opacity-100" : "opacity-70"
-          }`}
+      <ul className="max-w-48">
+        <Link href="#about-me" className="flex items-center group space-x-4">
+          <span
+            className={`group-hover:w-20 transition-all h-0.5 bg-indigo-600 group-hover:opacity-100 ${
+              active === 1 ? "w-20 opacity-100" : "w-8 opacity-70"
+            }`}
+          />
+          <span
+            className={`w-auto transition-transform group-hover:opacity-100 ${
+              active === 1 ? "opacity-100" : "opacity-70"
+            }`}
+          >
+            About me
+          </span>
+        </Link>
+      </ul>
+      <ul className="max-w-48">
+        <Link
+          href="#experience"
+          className="flex w-auto items-center space-x-2 group"
         >
-          About me
-        </span>
-      </Link>
-      <Link href="#experience" className="flex items-center space-x-2 group">
-        <span
-          className={`group-hover:w-20 group-hover:h-1 transition-all bg-indigo-600 group-hover:opacity-100 ${
-            active === 2 ? "w-20 h-1 opacity-100" : "w-8 h-0.5 opacity-70"
-          }`}
-        />
-        <span
-          className={`transition-transform group-hover:opacity-100 ${
-            active === 2 ? "opacity-100" : "opacity-70"
-          }`}
-        >
-          Experience
-        </span>
-      </Link>
-      <Link href="#projects" className="flex items-center space-x-2 group">
-        <span
-          className={`group-hover:w-20 group-hover:h-1 transition-all bg-indigo-600 group-hover:opacity-100 ${
-            active === 3 ? "w-20 h-1 opacity-100" : "w-8 h-0.5 opacity-70"
-          }`}
-        />
-        <span
-          className={`transition-transform group-hover:opacity-100 ${
-            active === 3 ? "opacity-100" : "opacity-70"
-          }`}
-        >
-          Projects
-        </span>
-      </Link>
+          <span
+            className={`group-hover:w-20 transition-all h-0.5 bg-indigo-600 group-hover:opacity-100 ${
+              active === 2 ? "w-20 opacity-100" : "w-8 opacity-70"
+            }`}
+          />
+          <span
+            className={`transition-transform group-hover:opacity-100 ${
+              active === 2 ? "opacity-100" : "opacity-70"
+            }`}
+          >
+            Experience
+          </span>
+        </Link>
+      </ul>
+      <ul className="max-w-48">
+        <Link href="#projects" className="flex items-center space-x-2 group">
+          <span
+            className={`group-hover:w-20 transition-all h-0.5 bg-indigo-600 group-hover:opacity-100 ${
+              active === 3 ? "w-20 opacity-100" : "w-8 opacity-70"
+            }`}
+          />
+          <span
+            className={`transition-transform group-hover:opacity-100 ${
+              active === 3 ? "opacity-100" : "opacity-70"
+            }`}
+          >
+            Projects
+          </span>
+        </Link>
+      </ul>
     </nav>
   );
 };
